e6dbef02e7a33ed1c3661548e440a62d83598147
jujiro88 <carlosjcjb@hotmail.com>
2021-04-18 21:23:12 -0600

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -17,15 +17,11 @@ namespace API
     public class Startup
     {
         private readonly IConfiguration _config;
-
         public Startup(IConfiguration config)
         {
             _config = config;
 
         }
-
-         
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -35,6 +31,7 @@ namespace API
 
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +45,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
